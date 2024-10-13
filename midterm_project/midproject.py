7cc9c1b43188557ed2305d4e88b145f531a66689
madhav259 <167551329+madhav259@users.noreply.github.com>
2024-10-13 19:48:11 +0530

    renamed the variables in python file
    rename the variables

--- midterm_project/midproject.py
+++ midterm_project/midproject.py
@@ -1,20 +1,22 @@
 import json
 
 def read_json(filename):
-    with open(filename, 'r') as f,open('customers.json','w') as f1,open('items.json','w')as f2:
+    with open(filename, 'r') as f:
         reads= json.load(f)
-        dict1={}
-        dict2={}
+        customer={}
+        items={}
         for item in range(len(reads)):
-            dict1[reads[item]['phone']]=reads[item]['name']
+            customer[reads[item]['phone']]=reads[item]['name']
             for item_name in reads[item]['items']:
-                if item_name['name'] not in dict2:
-                    dict2[item_name['name']]={}
-                    dict2[item_name['name']]['price']=item_name['price']
-                    dict2[item_name['name']]['orders']=1
+                if item_name['name'] not in items:
+                    items[item_name['name']]={}
+                    items[item_name['name']]['price']=item_name['price']
+                    items[item_name['name']]['orders']=1
                 else:
-                    dict2[item_name['name']]['orders'] += 1
-        f1.write(json.dumps(dict1))
-        f2.write(json.dumps(dict2))        
+                    items[item_name['name']]['orders'] += 1
+    with open('customers.json','w') as f1:
+        f1.write(json.dumps(customer))
+    with open('items.json','w') as f2:
+        f2.write(json.dumps(items))        
 
 read_json('example_orders.json')
